8abc07d01fcd81675184a152704c1ccba324ee36
freddyrios23 <fre.rios@duocuc.cl>
2026-05-10 20:29:12 -0400

    actualización en ColoresController

--- src/main/java/com/shoes_shop/controller/ColoresController.java
+++ src/main/java/com/shoes_shop/controller/ColoresController.java
@@ -9,44 +9,28 @@ import org.springframework.web.bind.annotation.*;
 
 import com.shoes_shop.model.Color;
 import com.shoes_shop.service.ColorService;
+import com.shoes_shop.service.ColoresService;
 
 @RestController
-@RequestMapping("/api/v1/color")
-public class ColorController {
+@RequestMapping("/api/v1/colores")
+public class ColoresController {
 
     @Autowired
-    private ColorService colorService;
+    private ColoresService coloresService;
 
     @GetMapping
     public ResponseEntity<List<Color>> todosLosColores() {
-        List<Color> colores = colorService.obtenerTodos();
+        List<Color> colores = coloresService.obtenerTodos();
 
         if (colores.isEmpty()) {
             return new ResponseEntity<>(HttpStatus.NO_CONTENT);
         }
         return new ResponseEntity<>(colores, HttpStatus.OK);
     }
-
-    @GetMapping("/{id}")
-    public ResponseEntity<?> buscarPorId(@PathVariable Integer id) {
-        return new ResponseEntity<>(colorService.buscarPorId(id), HttpStatus.OK);
-    }
-
-
+    
     @PostMapping
     public ResponseEntity<?> agregarColor(@RequestBody Color color) {
-        return new ResponseEntity<>(colorService.guardarColor(color), HttpStatus.CREATED);
+        return new ResponseEntity<>(coloresService.guardarColor(color), HttpStatus.CREATED);
     }
 
-
-    @PutMapping("/{id}")
-    public ResponseEntity<?> actualizar(@PathVariable Integer id, @RequestBody Color color) {
-        return new ResponseEntity<>(colorService.actualizarColor(id, color), HttpStatus.OK);
-    }
-
-
-    @DeleteMapping("/{id}")
-    public ResponseEntity<?> eliminar(@PathVariable Integer id) {
-        return new ResponseEntity<>(colorService.eliminarColor(id), HttpStatus.OK);
-    }
 }
