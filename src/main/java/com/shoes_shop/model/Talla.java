42d6ad36c3294d2135d45451db3c8450f45c3c6c
freddyrios23 <fre.rios@duocuc.cl>
2026-05-10 23:30:28 -0400

    actualizacion main

--- src/main/java/com/shoes_shop/model/Talla.java
+++ src/main/java/com/shoes_shop/model/Talla.java
@@ -1,14 +1,22 @@
 package com.shoes_shop.model;
 
 import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 import jakarta.validation.constraints.Min;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
 @Table(name = "tallas")
 public class Talla {
     @Id
